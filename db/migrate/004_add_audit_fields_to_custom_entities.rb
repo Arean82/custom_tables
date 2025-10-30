141f2b0e72e87195b747a28c560024367370f8db
arean82 <arean82@gmail.com>
2025-10-30 16:12:13 +0530

    DuplicateColumn issue fixed

--- db/migrate/004_add_audit_fields_to_custom_entities.rb
+++ db/migrate/004_add_audit_fields_to_custom_entities.rb
@@ -1,8 +1,20 @@
 class AddAuditFieldsToCustomEntities < ActiveRecord::Migration[4.2]
   def change
-    add_column :custom_entities, :author_id, :integer
-    add_column :custom_entities, :updated_by_id, :integer
-    add_foreign_key :custom_entities, :users, column: :author_id
-    add_foreign_key :custom_entities, :users, column: :updated_by_id
+    unless column_exists?(:custom_entities, :author_id)
+      add_column :custom_entities, :author_id, :integer
+    end
+
+    unless column_exists?(:custom_entities, :updated_by_id)
+      add_column :custom_entities, :updated_by_id, :integer
+    end
+
+    # Add foreign keys only if not already present
+    unless foreign_key_exists?(:custom_entities, :users, column: :author_id)
+      add_foreign_key :custom_entities, :users, column: :author_id
+    end
+
+    unless foreign_key_exists?(:custom_entities, :users, column: :updated_by_id)
+      add_foreign_key :custom_entities, :users, column: :updated_by_id
+    end
   end
 end
